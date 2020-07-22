abca5111b4093c86200ec583eb4265579bf8afab
Anik Sarker <aniksarker1612@gmail.com>
2020-07-23 00:52:11 +0800

    preloader added

--- theme.js
+++ theme.js
@@ -1,6 +1,13 @@
+var preloader = document.getElementById('pre-loader');
+    function onloadFunc(){
+    preloader.style.display = 'none';
+    
+}
+
 var themeBtn = document.getElementById('themeBtn');
 themeBtn.addEventListener('click', function(){
     //set background color white to black
     document.body.style.backgroundColor = '#1d1d1d';
     document.body.style.color = '#ffffff';
 })
+
